225b2e23a8f85ecd5f79c38270647f41358a5ae4
pivnati <natanmuleta911@gmail.com>
2025-07-17 02:08:10 -0700

    import all the components in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import GetApp from "@/components/GetApp";
+import Features from "@/components/Features";
 export default function Home() {
-  return ( 
+  return (
     <>
-       
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
     </>
   );
 }
