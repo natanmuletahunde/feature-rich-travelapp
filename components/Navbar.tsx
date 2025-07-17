aec11539bcbc4f6812e81243080bed63d8d2af9e
pivnati <natanmuleta911@gmail.com>
2025-07-17 00:30:59 -0700

    make the navlinks display in the home page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
 import Image from "next/image";
+import { NAV_LINKS } from "@/constants";
+import { link } from "fs";
 
 const Navbar = () => {
   return (
     <div className="  flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
-        <ul></ul>
+        <ul className="hidden h-full gap-12 lg:flex">
+          {NAV_LINKS.map((link)=>(
+            <Link href={link.href} key={link.key}>
+            {link.label}</Link>
+          ))}
+
+        </ul>
       </Link>
     </div>
   );
