5c5b1fe3abe045a57717f1900d93e1b4ce0173fb
pivnati <natanmuleta911@gmail.com>
2025-07-16 23:55:28 -0700

    add the modify the nav bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const Navbar = () => {
   return (
-    <div  className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
-       <Link >
-       </Link>
+    <div className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
